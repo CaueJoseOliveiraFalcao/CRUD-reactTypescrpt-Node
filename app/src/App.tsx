ab4a051f08729c9e0059876ad0a1e9ee1a5ac774
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-04-27 12:24:21 -0400

    gg

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import  RoutesR  from './routes';
+function App() {
+  return (
+    <BrowserRouter>
+      <RoutesR/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
